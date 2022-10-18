b98f09d4337e9fe074599103a300943af615a092
Julian Ismael Luna  Arecha <julian.arecha@gmail.com>
2022-10-18 03:31:09 -0300

    Agrego acordeon, ofertas y otrros detalles

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,11 +1,11 @@
-fetch("https://fakestoreapi.com/products/category/electronics")
+fetch("https://fakestoreapi.com/products/category/electronics?limit=3")
   .then((res) => res.json())
   .then((json) => {
-    let outputspans = ``
+    let outputspans = ``;
     json.forEach((element) => {
       outputspans += `
                 <span class="producto" id="idproducto">
-                  <div class="imagendelproducto"><img src="${element.image}" alt="${element.description}" id="idimagenproducto"></div>
+                  <div class="imagendelproducto"><img class="marcoimagen" src="${element.image}" alt="${element.description}" id="idimagenproducto"></div>
                   <div class="preciodelproducto" id="idpreciodelproducto"> $ ${element.price}</div>
                   <div class="titulodeproducto" id="idtitulodeproducto">${element.title}</div>
                   <div class="descripciondelproducto" id="iddescripciondelproducto">${element.description} 
@@ -14,8 +14,61 @@ fetch("https://fakestoreapi.com/products/category/electronics")
                 </span>
 
                 `;
-    })
+    });
     document.getElementById("idproductosindex").innerHTML = outputspans;
   });
 
- 
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((json) => {
+    let outputspans = ``;
+    let i = 1;
+    for (element of json) {
+      if (element.rating.rate >= 3) {
+        let style = ``;
+        if (i != 1) {
+          style = `style="display: none"`;
+        } else {
+          outputspans = `<div class="titulofav">Favoritos de los clientes</div>`;
+        }
+        outputspans += `
+      <div class="sectionfav">
+                <div class="titulodeproducto itema half pointer" onclick="toggleDisplay('idmenu${i}')">
+                  <span class="producttitlefav"> ${element.title} </span>
+                  <span class="ratingandcount"> ${
+                    element.rating.rate * 20
+                  }% de ${element.rating.count} votos </span>                  
+                </div>
+                <div class="productof" ${style} id="idmenu${i}">  
+                  <p>
+                    <div class="imagendelproductofav half"><img class="marcoimagen" src="${
+                      element.image
+                    }" alt="${
+          element.description
+        }" id="idimagenproducto"> <span class="preciodelproductofav"> $ ${
+          element.price
+        }</span></div>
+                    <div class="descripciondelproducto half">${
+                      element.description
+                    }</div>
+                  </p>  
+                </div>  
+      </div>
+                `;
+        i++;
+      }
+      if (i >= 15) {
+        break;
+      }
+    }
+
+    document.getElementById("idfavoritos").innerHTML = outputspans;
+  });
+
+function toggleDisplay(element) {
+  if(document.getElementById(element).style.display == "none"){
+  document.getElementById(element).style.display = "initial";
+  } else {
+    document.getElementById(element).style.display = "none";
+  }
+}
