8c6d238f453533faccf661d9a4f2715e82601885
Julian Ismael Luna  Arecha <jarecha@comviva.com>
2022-10-19 03:34:13 -0300

    Orden por modulos Vanilla JS

---
+++ assets/js/index.js
@@ -0,0 +1,96 @@
+function toggleDisplay(element) {
+    if(document.getElementById(element).style.display == "none"){
+    document.getElementById(element).style.display = "initial";
+    } else {
+      document.getElementById(element).style.display = "none";
+    }
+  }
+
+fetch("https://fakestoreapi.com/products?limit=3")
+  .then((res) => res.json())
+  .then((json) => {
+    let outputdivs = ``;
+    json.forEach((element) => {
+      outputdivs += `
+          <div class="producto" id="idcarta">
+            <div><img class="marcoimagen" src="${element.image}" alt="${element.description}" id="idimagenproductocarta"></div>
+            <div id="idpreciodelproductocarta"> $ ${element.price}</div>
+            <div id="idtitulodeproductocarta">${element.title}</div>
+          </div>
+      `;
+    });
+    document.getElementById("idcartascontenido").innerHTML = outputdivs;
+  });
+
+function classToggle() {
+  var el = document.querySelector(".cartascontenido");
+  el.classList.toggle("step-animation");
+}
+
+
+fetch("https://fakestoreapi.com/products/category/electronics?limit=3")
+  .then((res) => res.json())
+  .then((json) => {
+    let outputspans = ``;
+    json.forEach((element) => {
+      outputspans += `
+                <span class="producto" id="idproducto">
+                  <div class="imagendelproducto"><img class="marcoimagen" src="${element.image}" alt="${element.description}" id="idimagenproducto"></div>
+                  <div class="preciodelproducto" id="idpreciodelproducto"> $ ${element.price}</div>
+                  <div class="titulodeproducto" id="idtitulodeproducto">${element.title}</div>
+                  <div class="descripciondelproducto" id="iddescripciondelproducto">${element.description} 
+                  </div>
+                  <span id="expand-sizer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false">Ver mas</span>
+                </span>
+
+                `;
+    });
+    document.getElementById("idproductosindex").innerHTML = outputspans;
+  });
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((json) => {
+    let outputspans = ``;
+    let i = 1;
+    for (element of json) {
+      if (element.rating.rate >= 3) {
+        let style = ``;
+        if (i != 1) {
+          style = `style="display: none"`;
+        } else {
+          outputspans = `<div class="titulofav">Favoritos de los clientes</div>`;
+        }
+        outputspans += `
+      <div class="sectionfav">
+                <div class="titulodeproducto itema half pointer" onclick="toggleDisplay('idmenu${i}')">
+                  <span class="producttitlefav"> ${element.title} </span>
+                  <span class="ratingandcount"> ${
+                    element.rating.rate * 20
+                  }% de ${element.rating.count} votos </span>                  
+                </div>
+                <div class="productof" ${style} id="idmenu${i}">  
+                  <p>
+                    <div class="imagendelproductofav half"><img class="marcoimagen" src="${
+                      element.image
+                    }" alt="${
+          element.description
+        }" id="idimagenproducto"> <span class="preciodelproductofav"> $ ${
+          element.price
+        }</span></div>
+                    <div class="descripciondelproducto half">${
+                      element.description
+                    }</div>
+                  </p>  
+                </div>  
+      </div>
+                `;
+        i++;
+      }
+      if (i >= 15) {
+        break;
+      }
+    }
+
+    document.getElementById("idfavoritos").innerHTML = outputspans;
+  });
--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,9 +1,32 @@
-fetch("https://fakestoreapi.com/products/category/electronics?limit=3")
+import precisePrice from "./comun.js";
+import precise from "./comun.js";
+
+fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((json) => {
     let outputspans = ``;
     json.forEach((element) => {
-      outputspans += `
+      if (element.rating.rate <= 3 && element.category != "electronics") {
+        let descuento = precise(
+          element.rating.rate * 10 >= 35 ? 35 : element.rating.rate * 10
+        );
+        let precio = precisePrice(element.price - (element.price * descuento) / 100);
+        outputspans += `
+                <span class="producto" id="idproducto">
+                  <div class="imagendelproducto"><img class="marcoimagen" src="${element.image}" alt="${element.description}" id="idimagenproducto"></div>
+                  <div class="preciodelproducto" id="idpreciodelproducto"> 
+                  <span class="oldprice"> $ ${element.price} </span>
+                  <span class="newprice"> $ ${precio} </span>
+                  <span class="discount"> -${descuento} %</span>
+                  </div> 
+                  <div class="titulodeproducto" id="idtitulodeproducto">${element.title}</div>
+                  <div class="descripciondelproducto" id="iddescripciondelproducto">${element.description} 
+                  </div>
+                  <span id="expand-sizer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false">Ver mas</span>
+                </span>
+                `;
+      } else {
+        outputspans += `
                 <span class="producto" id="idproducto">
                   <div class="imagendelproducto"><img class="marcoimagen" src="${element.image}" alt="${element.description}" id="idimagenproducto"></div>
                   <div class="preciodelproducto" id="idpreciodelproducto"> $ ${element.price}</div>
@@ -14,61 +37,7 @@ fetch("https://fakestoreapi.com/products/category/electronics?limit=3")
                 </span>
 
                 `;
+      }
     });
-    document.getElementById("idproductosindex").innerHTML = outputspans;
+    document.getElementById("idlistadoproductos").innerHTML = outputspans;
   });
-
-fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((json) => {
-    let outputspans = ``;
-    let i = 1;
-    for (element of json) {
-      if (element.rating.rate >= 3) {
-        let style = ``;
-        if (i != 1) {
-          style = `style="display: none"`;
-        } else {
-          outputspans = `<div class="titulofav">Favoritos de los clientes</div>`;
-        }
-        outputspans += `
-      <div class="sectionfav">
-                <div class="titulodeproducto itema half pointer" onclick="toggleDisplay('idmenu${i}')">
-                  <span class="producttitlefav"> ${element.title} </span>
-                  <span class="ratingandcount"> ${
-                    element.rating.rate * 20
-                  }% de ${element.rating.count} votos </span>                  
-                </div>
-                <div class="productof" ${style} id="idmenu${i}">  
-                  <p>
-                    <div class="imagendelproductofav half"><img class="marcoimagen" src="${
-                      element.image
-                    }" alt="${
-          element.description
-        }" id="idimagenproducto"> <span class="preciodelproductofav"> $ ${
-          element.price
-        }</span></div>
-                    <div class="descripciondelproducto half">${
-                      element.description
-                    }</div>
-                  </p>  
-                </div>  
-      </div>
-                `;
-        i++;
-      }
-      if (i >= 15) {
-        break;
-      }
-    }
-
-    document.getElementById("idfavoritos").innerHTML = outputspans;
-  });
-
-function toggleDisplay(element) {
-  if(document.getElementById(element).style.display == "none"){
-  document.getElementById(element).style.display = "initial";
-  } else {
-    document.getElementById(element).style.display = "none";
-  }
-}
