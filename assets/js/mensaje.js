21d15a32748ed47cee499e9de8ee45d081310cbe
Julian Ismael Luna  Arecha <julian.arecha@gmail.com>
2022-10-31 02:54:55 -0300

    Limites en javascript con regx mensaje

--- assets/js/mensaje.js
+++ assets/js/mensaje.js
@@ -19,5 +19,44 @@ async function handleFormSubmit(event) {
     });
 }
 
+function validate() {
+    let phoneRegx = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/ ;
+    let nameRegx = /^[a-zA-Z ]{2,30}$/ ;
+    let emailRegx = /^[a-z0-9.]{1,64}@[a-z0-9.]{1,64}$/i ;
+    let asuntoRegx = /^(?=.{0,50}$)\d+(-?\d+)?$/ ;
+    let mensajeRegx = /^(?=.{0,1200}$)\d+(-?\d+)?$/ ;
+    if(!document.mensaje.sourcePhone.value.match(phoneRegx)) {
+       alert( "Por favor corregi tu telefono." );
+       document.mensaje.sourcePhone.focus() ;
+       return false;
+    }
+    if(!document.mensaje.sourceLastName.value.match(nameRegx)) {
+        alert( "Por favor corregi tu apellido." );
+        document.mensaje.sourceLastName.focus() ;
+        return false;
+     }
+     if(!document.mensaje.sourceFirstName.value.match(nameRegx)) {
+        alert( "Por favor corregi tu nombre." );
+        document.mensaje.sourceFirstName.focus() ;
+        return false;
+     }
+     if(!document.mensaje.sourceEmail.value.match(emailRegx)) {
+        alert( "Por favor corregi tu email." );
+        document.mensaje.sourceEmail.focus() ;
+        return false;
+     }
+     if(!document.mensaje.sourceMessageTitle.value.match(asuntoRegx)) {
+        alert( "El asunto no debe tener mas de 50 caracteres." );
+        document.mensaje.sourceMessageTitle.focus() ;
+        return false;
+     }
+     if(!document.mensaje.sourceMessage.value.match(mensajeRegx)) {
+        alert( "El mensaje no debe tener mas de 1200 caracteres." );
+        document.mensaje.sourceMessage.focus() ;
+        return false;
+     }
+    return( true );
+ }
+
 const form = document.querySelector('.contact-form');
 form.addEventListener('submit', handleFormSubmit);
